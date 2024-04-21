b55787be2e7f119006e39f3b1687332691ecad41
PrintHolamundo <jdohca@gmail.com>
2024-04-21 16:40:51 -0600

    update

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ chown -R $username:$username /home/$username
 # Installing Essential Programs 
 nala install feh kitty rofi picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pipewire wireplumber pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev zoxide xdg-utils -y
 # Installing Other less important Programs
-nala install neofetch flameshot psmisc mangohud vim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm flatpak obs-studio remmina copyq btop intel-microcode neovim telegram-desktop brave-browser spotify-client -y
+nala install neofetch flameshot psmisc mangohud vim lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm flatpak obs-studio remmina copyq btop intel-microcode neovim telegram-desktop brave-browser spotify-client v4l2loopback-dkms -y
 
 #Setup Flatpak 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
