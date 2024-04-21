f6ff8710f552c60cf340154f0ef061d90d2fe134
PrintHolamundo <jdohca@gmail.com>
2024-04-21 15:54:56 -0600

    update

--- install.sh
+++ install.sh
@@ -27,6 +27,8 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] http
 curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
+apt update
+
 #Setup Flatpak 
 apt install flatpak brave-browser  obs-studio remmina copyq btop intel-microcode spotify-client neovim telegram-desktop
 
