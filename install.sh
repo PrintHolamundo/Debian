8b6afb253300e7d6b921c03962387ba40627f47c
PrintHolamundo <jdohca@gmail.com>
2024-04-22 00:18:22 -0600

    [ADD] Theme zsh

--- install.sh
+++ install.sh
@@ -116,4 +116,5 @@ cd $builddir
 bash scripts/usenala
 
 # Oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
