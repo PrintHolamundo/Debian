376b22797d1fdd5051fc262eebe472969ca6c20d
PrintHolamundo <jdohca@gmail.com>
2024-04-22 00:50:42 -0600

    [UPDATE] remove spotify install

--- install.sh
+++ install.sh
@@ -27,11 +27,6 @@ sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code -y
 
-#Spotify
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client -y
-
 #Brave-Browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
